3424d27e208bff1108fb09704b8563cd58384c60
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2009-05-18 10:09:18 +0000

    - Add "Expires" to response header.  - When cannot fetch a given feed, reuse stale cache for improving availability.
    git-svn-id: http://feed-tagcloud-appengine.googlecode.com/svn/trunk@11 4728c604-38a3-11de-8ef0-0f53c1b7e458

--- feed_tagcloud.py
+++ feed_tagcloud.py
@@ -26,33 +26,45 @@ class FeedTagCloudJsonHandler(webapp.RequestHandler):
       if cache:
         expires = cache.last_modified + datetime.timedelta(hours=1)
         if expires > datetime.datetime.now():
-          self.response.out.write(cache.json)
           logging.info("Succeeded to get feed from cache: %s" % feed_url)
+
+          self.response.headers.add_header('Expires', expires.strftime('%d %b %Y %H:%M:%S GMT'))
+          self.response.out.write(cache.json)
           return
     except Exception, e:
       logging.error(e)
       pass
 
+    json_text = ''
+    if cache:
+      json_text = cache.json
+
     try:
       feed = feedparser.parse(feed_url)
+#      if feed.bozo:
+#        raise Exception, "Failed to fetch feed: %s" % feed_url
       logging.info("Succeeded to parse feed: %s" % feed_url)
+
+      tag_cloud = {}
+      for entry in feed.entries:
+        if not 'tags' in entry:
+          continue
+        for tag in entry.tags:
+          tag_name = tag.term
+          if tag_name in tag_cloud:
+            tag_cloud[tag_name] += 1
+          else:
+            tag_cloud[tag_name] = 1
+      json_text = simplejson.dumps(tag_cloud, ensure_ascii=False)
     except Exception, e:
       logging.error(e)
       logging.info("Failed to parse feed: %s" % feed_url)
       pass
 
-    tag_cloud = {}
-    for entry in feed.entries:
-      if not 'tags' in entry:
-        continue
-      for tag in entry.tags:
-        tag_name = tag.term
-        if tag_name in tag_cloud:
-          tag_cloud[tag_name] += 1
-        else:
-          tag_cloud[tag_name] = 1
+#    if not json_text:
+#      self.error(500)
+#      return
 
-    json_text = simplejson.dumps(tag_cloud, ensure_ascii=False)
     try:
       if cache:
         cache.json = json_text
@@ -60,12 +72,15 @@ class FeedTagCloudJsonHandler(webapp.RequestHandler):
         cache = FeedTagCloudCache(url=feed_url, json=json_text)
       cache.put()
       logging.info("Succeeded to cache feed: %s" % feed_url)
+
+      expires = cache.last_modified + datetime.timedelta(hours=1)
+      self.response.headers.add_header('Expires', expires.strftime('%d %b %Y %H:%M:%S GMT'))
+      self.response.out.write(json_text)
+      return
     except Exception, e:
       logging.error(e)
       logging.info("Failed to cache feed: %s" % feed_url)
-      pass
-
-    self.response.out.write(json_text)
+      self.error(500)
 
 class FeedTagCloudHandler(webapp.RequestHandler):
   def get(self):
